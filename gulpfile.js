8efda9070c497cd7034b0bd51239f789dc741ba4
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2017-10-05 09:11:34 -0300

    add task to create a static server and livereload #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp =  require('gulp');
 var pug =  require('gulp-pug');
 var stylus =  require('gulp-stylus');
+var connect =  require('gulp-connect');
 
 gulp.task('pug', function(){
   gulp.src('./src/*.pug')
       .pipe(pug())
-      .pipe(gulp.dest('./out'));
+      .pipe(gulp.dest('./out'))
+      .pipe(connect.reload());
 });
 
 
 gulp.task('stylus', function(){
   gulp.src('./src/assets/styles/*.styl')
       .pipe(stylus())
-      .pipe(gulp.dest('./out/assets/styles'));
+      .pipe(gulp.dest('./out/assets/styles'))
+      .pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
@@ -20,4 +23,13 @@ gulp.task('watch', function(){
   gulp.watch(['./src/assets/styles/*.styl'], ['stylus']);
 });
 
+gulp.task('serve', function(){
+  connect.server({
+    root: './out',
+    livereload: true,
+    port: 3210
+  });
+});
+
 gulp.task('build', ['pug','stylus']);
+gulp.task('server',['serve','watch']);
