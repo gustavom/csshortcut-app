eaea02dcaee271893db82883f6ffc569335c0d1a
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2017-10-05 09:25:11 -0300

    Base do gulp file ok close #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-var gulp =  require('gulp');
-var pug =  require('gulp-pug');
-var stylus =  require('gulp-stylus');
-var connect =  require('gulp-connect');
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var connect = require('gulp-connect');
 
-gulp.task('pug', function(){
+gulp.task('pug', function () {
   gulp.src('./src/*.pug')
-      .pipe(pug())
-      .pipe(gulp.dest('./out'))
-      .pipe(connect.reload());
+    .pipe(pug())
+    .pipe(gulp.dest('./out'))
+    .pipe(connect.reload());
 });
 
 
-gulp.task('stylus', function(){
+gulp.task('stylus', function () {
   gulp.src('./src/assets/styles/*.styl')
-      .pipe(stylus())
-      .pipe(gulp.dest('./out/assets/styles'))
-      .pipe(connect.reload());
+    .pipe(stylus())
+    .pipe(gulp.dest('./out/assets/styles'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch(['./src/*.pug'], ['pug']);
   gulp.watch(['./src/assets/styles/*.styl'], ['stylus']);
 });
 
-gulp.task('serve', function(){
+gulp.task('serve', function () {
   connect.server({
     root: './out',
     livereload: true,
@@ -31,5 +31,5 @@ gulp.task('serve', function(){
   });
 });
 
-gulp.task('build', ['pug','stylus']);
-gulp.task('server',['serve','watch']);
+gulp.task('build', ['pug', 'stylus']);
+gulp.task('server', ['serve', 'watch']);
